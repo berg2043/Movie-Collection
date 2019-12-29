62c02d7c6b8a2749e7fb1cc50db344cecdd36664
Phillip Berg <phillipb1991@msn.com>
2019-12-29 16:36:27 -0600

    set up base index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Reducers
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Sagas
+
+
+// Creates redux store
+const storeInstance = createStore(
+  combineReducers({
+
+  }), applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
