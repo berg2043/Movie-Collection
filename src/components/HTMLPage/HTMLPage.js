ca6b3630e0f8114429efa055d0b0936950612199
Phillip Berg <phillipb1991@msn.com>
2019-12-29 17:04:43 -0600

    started base form on html page

---
+++ src/components/HTMLPage/HTMLPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HTMLForm(){
+  return(
+    <div>
+      <form>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default HTMLForm;
