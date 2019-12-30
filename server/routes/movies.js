b2451edf282413632a3208c085df3f9a043a8fd5
Phillip Berg <phillipb1991@msn.com>
2019-12-29 21:34:55 -0600

    added Delete Route

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Get
 router.get('/', (req,res)=>{
-  const queryText = 'SELECT * FROM "movies" ORDER BY "name"'
+  const queryText = 'SELECT * FROM "movies" ORDER BY "name";';
   pool.query(queryText).then(result=>{
     res.send(result.rows);
   }).catch(err=>{
@@ -15,9 +15,20 @@ router.get('/', (req,res)=>{
 
 // POST
 router.post('/', (req,res)=>{
-  const queryText = `INSERT INTO "movies"("name", "release", "run_time") VALUES($1,$2,$3)`
+  const queryText = `INSERT INTO "movies"("name", "release", "run_time") VALUES($1,$2,$3);`;
   pool.query(queryText, [req.body.name, req.body.date, req.body.time]).then(result=>{
     res.sendStatus(201);
+  }).catch(err=>{
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
+// DELETE
+router.delete('/:id', (req,res)=>{
+  const queryText = `DELETE FROM "movies" WHERE "id" = $1;`;
+  pool.query(queryText, [req.params.id]).then(result=>{
+    res.sendStatus(200);
   }).catch(err=>{
     console.log(err);
     res.sendStatus(500);
