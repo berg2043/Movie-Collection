e0591457c68d4fe7a8ee03c15b37919f9a9a19a1
Phillip Berg <phillipb1991@msn.com>
2019-12-29 20:49:17 -0600

    updated Names

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import HTMLPage from './../HTMLPage/HTMLPage'
+import HTMLPage from '../MoviePage/MoviePage'
 
 function App() {
   return (
---
+++ src/components/MoviePage/MoviePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HTMLForm from "../MovieForm/MovieForm";
+
+function MoviePage(){
+  return(
+    <div>
+      <HTMLForm />
+    </div>
+  );
+};
+
+export default MoviePage;
