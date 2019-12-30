73dbdbe4b6111d92681273947b586a72a9d991d5
Phillip Berg <phillipb1991@msn.com>
2019-12-29 20:31:32 -0600

    setup movie route

---
+++ server/routes/movies.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('./../modules/pool');
+const router = express.Router();
+
+// Get
+router.get('/', (req,res)=>{
+  const queryText = 'SELECT * FROM "movies" ORDER BY "name"'
+  pool.query(queryText).then(result=>{
+    res.send(result.rows);
+  }).catch(err=>{
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
+// POST
+router.post('/', (req,res)=>{
+  const queryText = `INSERT INTO "movies"("name", "release", "run-time") VALUES($1,$2,$3)`
+  pool.query(queryText, [req.body.name, req.body.date, req.body.time]).then(result=>{
+    res.sendStatus(201);
+  }).catch(err=>{
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const movies = require('./routes/movies')
 
 // Middleware
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('build'));
 
 // Routes
+app.use('api/movies', movies)
 
 // Start Server
 app.listen(port, function () {
