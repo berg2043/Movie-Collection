fabf083b25d4756d97af4d76e5e3823553c4e84c
Phillip Berg <phillipb1991@msn.com>
2019-12-29 20:19:53 -0600

    set up get and post movie calls

--- src/index.js
+++ src/index.js
@@ -5,17 +5,33 @@ import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery } from 'redux-saga/effects'
 import logger from 'redux-logger';
 // Reducers
+import movieList from './reducers/movieList';
 
 // Sagas
+import getMovies from './sagas/getMovies';
+import postMovie from './sagas/postMovie';
 
+function* rootSaga(){
+  yield takeEvery('GET_MOVIES', getMovies);
+  yield takeEvery('POST_MOVIE', postMovie);
+}
+
+// Create sagamiddleware
+const sagaMiddleware = createSagaMiddleware();
 
 // Creates redux store
 const storeInstance = createStore(
   combineReducers({
-  }), applyMiddleware(logger)
+    movieList
+  }), applyMiddleware(logger, sagaMiddleware)
 );
+
+// Start sagaMiddleware
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 
