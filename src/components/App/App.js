a7defa9f07c43fa7fe226e71167ab925dc55fc94
Phillip Berg <phillipb1991@msn.com>
2019-12-29 22:08:26 -0600

    added Genre page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import HTMLPage from '../MoviePage/MoviePage'
 import Nav from '../Nav/Nav';
+import MoviePage from '../MoviePage/MoviePage'
+import GenrePage from '../GenrePage/GenrePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Nav/>
-        <Route path='/' render={(props)=><HTMLPage {...props}/>}/>
+        <Route exact path='/' render={(props)=><MoviePage {...props}/>}/>
+        <Route exact path='/genres' render={(props)=><GenrePage {...props}/>}/>
       </Router>
     </div>
   );
