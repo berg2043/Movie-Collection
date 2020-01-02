a5fc4e3b13ad7900fbdd189c3af2376232a051e9
Phillip Berg <phillipb1991@msn.com>
2020-01-01 20:59:54 -0600

    set up SQL transaction to add to multiple tables and get to get genres

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -4,7 +4,19 @@ const router = express.Router();
 
 // Get
 router.get('/', (req,res)=>{
-  const queryText = 'SELECT * FROM "movies" ORDER BY "name";';
+  const queryText = 
+      `SELECT 
+        "movies"."id", 
+        "movies"."name", 
+        "movies"."description", 
+        "movies"."release", 
+        "movies"."run_time", 
+        "movies"."votes",
+        "genres"."project"
+      FROM "movies"
+      LEFT JOIN "m_g" on "movies"."id" = "m_g"."m_id"
+      LEFT JOIN "genres" on "m_g"."g_id" = "genres"."id"
+      ORDER BY "movies"."name";`;
   pool.query(queryText).then(result=>{
     res.send(result.rows);
   }).catch(err=>{
@@ -14,14 +26,41 @@ router.get('/', (req,res)=>{
 });
 
 // POST
-router.post('/', (req,res)=>{
-  const queryText = `INSERT INTO "movies"("name", "release", "run_time") VALUES($1,$2,$3);`;
-  pool.query(queryText, [req.body.name, req.body.date, req.body.time]).then(result=>{
+router.post('/', async (req,res)=>{
+  // Sets up initial connection
+  const client = await pool.connect();
+  try{
+    const {
+      name,
+      genre,
+      date,
+      time
+    } = req.body;
+    await client.query('BEGIN');
+    // Inserts movie and gets it's ID
+    const movieInsertResults = await client.query(
+      `INSERT INTO "movies"("name", "release", "run_time") VALUES($1,$2,$3) RETURNING "id";`,
+      [name, date, time]
+    );
+    const movieId = movieInsertResults.rows[0].id;
+    // Gets the genres ID
+    const getGenreId = await client.query(
+      `SELECT * FROM "genres" WHERE "project" = $1;`,
+      [genre]
+    );
+    const genreId = getGenreId.rows[0].id;
+    // Adds both IDs into relational database
+    await client.query(`INSERT INTO "m_g"("m_id", "g_id") VALUES($1, $2)`, [movieId, genreId]);
+    // Commits the inserts if no errors
+    await client.query('COMMIT');
     res.sendStatus(201);
-  }).catch(err=>{
-    console.log(err);
+  } catch (error) {
+    await client.query('ROLLBACK')
+    console.log('Error POST /api/movies', error);
     res.sendStatus(500);
-  });
+  } finally {
+    client.release()
+  }
 });
 
 // DELETE
