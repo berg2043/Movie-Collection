75c69640d0cd9ddc7d168893376e29878d757ac6
Phillip Berg <phillipb1991@msn.com>
2020-01-01 20:37:12 -0600

    added relational table m_g to get count of movies and remove for there as well

--- server/routes/genres.js
+++ server/routes/genres.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 // Get
 router.get('/', (req,res)=>{
-  const queryText = 'SELECT * FROM "genres" ORDER BY "project";';
+  const queryText = 
+      `SELECT "genres"."id", "genres"."project", COUNT("m_g"."g_id")
+      FROM "genres"
+      LEFT JOIN "m_g" on "genres"."id" = "m_g"."g_id"
+      GROUP BY "genres"."id"
+      ORDER BY "project";`;
   pool.query(queryText).then(result=>{
     res.send(result.rows);
   }).catch(err=>{
@@ -26,13 +31,14 @@ router.post('/', (req,res)=>{
 
 // DELETE
 router.delete('/:id', (req,res)=>{
-  const queryText = `DELETE FROM "genres" WHERE "id" = $1;`;
-  pool.query(queryText, [req.params.id]).then(result=>{
-    res.sendStatus(200);
-  }).catch(err=>{
+  const relationalText = `DELETE FROM "m_g" WHERE "g_id" = $1;`;
+  pool.query(relationalText, [req.params.id]).then(result=>{
+    const queryText = `DELETE FROM "genres" WHERE "id" = $1;`;
+    pool.query(queryText, [req.params.id]).then(result=>{
+      res.sendStatus(200);
+  })}).catch(err=>{
     console.log(err);
-    res.sendStatus(500);
-  });
-});
+    res.sendStatus(500);;
+})});
 
 module.exports = router;
